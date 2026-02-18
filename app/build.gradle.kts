1922cd06a9664663045a520b453caef42bc2918b
UchiElen <elensegman@gmail.com>
2026-02-18 12:50:29 +0100

    Firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.android.application")
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
 }
