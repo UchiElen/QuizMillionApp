00edf62cfb62f2f11de99d1d28f621a9752f3a69
ivanpasgar-ipg <ivanpasgar@gmail.com>
2026-04-24 03:41:28 +0200

    Limpieza de codigo

--- app/src/main/java/com/dam/quizmillionapp/RecordsUsuarioActivity.java
+++ app/src/main/java/com/dam/quizmillionapp/RecordsUsuarioActivity.java
@@ -7,9 +7,7 @@ import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.Button;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
